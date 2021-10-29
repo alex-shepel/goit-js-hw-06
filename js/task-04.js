7c537145bef3376467fca8090869f15506e4faa8
AlexandrShepel <iam.alex.shepel@gmail.com>
2021-10-29 19:56:49 +0300

    Do task-4

---
+++ js/task-04.js
@@ -0,0 +1,17 @@
+const refs = {
+	btnDecrement: document.querySelector('button[data-action="decrement"]'),
+	btnIncrement: document.querySelector('button[data-action="increment"]'),
+	spanValue: document.querySelector("#value"),
+};
+
+let counterValue = 0;
+
+const refreshIface = value => {
+	refs.spanValue.textContent = value;
+};
+
+const onClickBtnDecrement = () => refreshIface((counterValue -= 1));
+const onClickBtnIncrement = () => refreshIface((counterValue += 1));
+
+refs.btnDecrement.addEventListener("click", onClickBtnDecrement);
+refs.btnIncrement.addEventListener("click", onClickBtnIncrement);
