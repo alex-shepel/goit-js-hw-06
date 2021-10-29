5e0d15ebeec850d9a49b227c503bc14190c91ebf
AlexandrShepel <iam.alex.shepel@gmail.com>
2021-10-29 18:43:03 +0300

    Do task-1

--- js/task-01.js
+++ js/task-01.js
@@ -1,1 +1,9 @@
+const items = document.querySelector("#categories").children;
 
+console.log(`Number of categories: ${items.length}`);
+
+[...items].map(item => {
+	console.log("");
+	console.log(`Category: ${item.querySelector("h2").textContent}`);
+	console.log(`Elements: ${item.querySelector("ul").children.length}`);
+});
