2fd001f863e3d9b880882b6f0440243822efc36b
AlexandrShepel <iam.alex.shepel@gmail.com>
2021-10-29 18:59:37 +0300

    Do task-2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,12 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ["Potatoes", "Mushrooms", "Garlic", "Tomatos", "Herbs", "Condiments"];
+
+const list = document.querySelector("#ingredients");
+
+list.append(
+	...ingredients.map(ingridient => {
+		const item = document.createElement("li");
+		item.textContent = ingridient;
+		item.className = "item";
+		return item;
+	})
+);
