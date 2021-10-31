552d5f6d9be809cdc47de12d8ab2f2f915169f4b
AlexandrShepel <iam.alex.shepel@gmail.com>
2021-10-31 20:26:00 +0200

    Beautify task-10

--- js/task-10.js
+++ js/task-10.js
@@ -7,30 +7,32 @@ const refs = {
 
 const BOX_MIN_SIZE = 30;
 const BOX_SIZING_STEP = 10;
+const BOX_POSITION =
+	"display:flex; justify-content:center; align-items:center;";
 
 function getRandomHexColor() {
 	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const getColoredBoxWithSize = size => {
+const getColoredBoxWithSize = (size, innerBox) => {
 	const color = getRandomHexColor();
-	return `<div style="width:${size}px;height:${size}px;background-color:${color};"></div>`;
+	return `<div style="${BOX_POSITION} width:${size}px; height:${size}px; background-color:${color};">${innerBox}</div>`;
 };
 
 const createBoxes = amount => {
-	let size = BOX_MIN_SIZE;
 	let markdown = "";
 
 	for (let i = 0; i < amount; i += 1) {
-		markdown += getColoredBoxWithSize((size += BOX_SIZING_STEP));
+		markdown = getColoredBoxWithSize(
+			BOX_MIN_SIZE + BOX_SIZING_STEP * i,
+			markdown
+		);
 	}
 
 	return markdown;
 };
 
 const onClickCreate = () => {
-	console.log("refs.input.value :>> ", refs.input.value);
-	console.log("markdown: ", createBoxes(refs.input.value));
 	refs.boxesWrapper.innerHTML = createBoxes(refs.input.value);
 };
 
