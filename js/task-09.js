db929de93a18e408594540181097478da1ed5c12
AlexandrShepel <iam.alex.shepel@gmail.com>
2021-10-31 19:04:46 +0200

    Do task-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
+const refs = {
+	body: document.querySelector("body"),
+	output: document.querySelector(".color"),
+	button: document.querySelector(".change-color"),
+};
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const onClickBtn = () => {
+	const color = getRandomHexColor();
+	refs.body.style.backgroundColor = color;
+	refs.output.textContent = color;
+};
+
+refs.button.addEventListener("click", onClickBtn);
