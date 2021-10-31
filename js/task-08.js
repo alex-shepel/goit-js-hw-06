3d77fd6b82ed5003f35cf04bdaee3a21eb86c650
AlexandrShepel <iam.alex.shepel@gmail.com>
2021-10-31 18:50:45 +0200

    Do task-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const refs = {
+	form: document.querySelector(".login-form"),
+};
 
+const onSubmit = event => {
+	event.preventDefault();
+
+	const form = event.currentTarget;
+	const data = {};
+
+	[...form.elements]
+		.filter(elem => elem.tagName === "INPUT")
+		.map(elem => (data[elem.name] = elem.value));
+
+	if (Object.values(data).some(value => !value)) {
+		alert("Please, fill in all fields.");
+	} else {
+		console.log("data :>> ", data);
+		form.reset();
+	}
+};
+
+refs.form.addEventListener("submit", onSubmit);
